aae58cc3cad18ae6723e43d3f513bd8f82bfdacf
Pereyra Jose <pereyrajose848@gmail.com>
2023-01-25 20:23:12 -0300

    agregue sweetAlert funcional

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,13 +1,33 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import { consultarApi, deleteTareaApi } from "./helpers/queries";
+import Swal from "sweetalert2";
 
 const Items = ({ tarea, setTarea }) => {
   const borrarTarea = (()=>{
  deleteTareaApi(tarea.id).then((respuesta)=>{
-consultarApi().then((respuesta)=>{
-setTarea(respuesta)
-})
+  Swal.fire({
+    title: '¿Estas Seguro?',
+    text: "Una vez eliminado no podras recuperarlo",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: '¡Si, Eliminar!',
+    cancelButtonText: 'Mejor NO'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      consultarApi().then((respuesta)=>{
+        setTarea(respuesta)
+        })
+      Swal.fire(
+        '¡Eliminado!',
+        'Tarea Eliminada',
+        'success'
+      )
+    }
+  })
+
  })
   });
   return (
